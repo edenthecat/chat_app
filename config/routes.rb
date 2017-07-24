e7965377d73731f5f94fb6bd831cb750590cc529
Eden Rohatensky <edenrohatensky@gmail.com>
2017-07-24 19:41:22 -0400

    Fixed routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :conversations
-  resources :messages
-  resources :chat_boxes
+  resources :conversations do
+    resources :messages
+  end
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
