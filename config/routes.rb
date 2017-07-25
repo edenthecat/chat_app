16be2e393a9d9adf85d2a18f48151aca49111516
Eden Rohatensky <edenrohatensky@gmail.com>
2017-07-25 12:35:21 -0400

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :conversations do
     resources :messages, only: [:index, :new, :create, :destroy]
   end
+
+  get '/conversations/:id/refresh_messages', to: 'conversations#refresh_messages', as: 'refresh_messages'
   devise_for :users
-  root to: 'pages#home'
+  root to: 'conversations#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
