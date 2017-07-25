16c61b8b05dd77dfbe2bb672fba29a6864ebafbc
Eden Rohatensky <edenrohatensky@gmail.com>
2017-07-25 13:29:50 -0400

    Cleaned up messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,23 +1,20 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
+  before_action :set_conversation, only: [:new, :create]
 
   # GET /messages/new
   def new
-    @conversation = Conversation.find(params[:conversation_id])
     @message = Message.new
   end
 
   # POST /messages
   # POST /messages.json
   def create
-    @conversation = Conversation.find(params[:conversation_id])
     @message = Message.new(message_params)
     @message.user_id = current_user.id
     @message.conversation_id = @conversation.id
 
     if @message.save!
-      puts "Message ID:"
-      puts @message.id
       respond_to do |format|
         format.js
       end
@@ -38,6 +35,10 @@ class MessagesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_message
       @message = Message.find(params[:id])
+    end
+
+    def set_conversation
+      @conversation = Conversation.find(params[:conversation_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
